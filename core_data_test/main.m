e7390259ea37c42328289f4a5d8e9dd64abbbeac
Taito Lee <taito-lee@live.jp>
2015-09-06 00:16:35 +0900

    Initial Commit

---
+++ core_data_test/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  core_data_test
+//
+//  Created by Taito Lee on 2015/09/06.
+//  Copyright (c) 2015年 otita. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
